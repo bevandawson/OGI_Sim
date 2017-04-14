4fd83dfbc7aec3688bb14ac7cb840a532f55fbb3
Bevan Dawson <Bevan Dawson>
2017-04-14 15:11:30 -0500

    Initial commit

---
+++ src/resourceCard.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Bevan on 4/1/2017.
+ */
+
+public class resourceCard {
+    public static void main(String[] args) {
+        String cardType = args[0];
+        Integer cardId = Integer.parseInt(args[1]);
+
+
+        System.out.println(cardType);
+        System.out.println(cardId);
+    }
+}
